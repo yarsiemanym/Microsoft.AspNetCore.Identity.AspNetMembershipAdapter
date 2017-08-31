25c3bcf470427629f617e6b6fc4b1de2655df4cd
Ray Saltrelli <raymond.saltrelli@gmail.com>
2017-08-31 14:26:40 -0400

    Removed some code specific to my use case to make it more generic.

--- src/IServiceCollectionExtensions.cs
+++ src/IServiceCollectionExtensions.cs
@@ -1,25 +1,30 @@
-﻿using IdentityModel;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Microsoft.AspNetCore.Identity.AspNetMembershipAdapter
 {
     public static class IServiceCollectionExtensions
     {
-        public static IdentityBuilder AddAspNetMembershipAdaptor(this IServiceCollection services, string connectionString)
+        public static IdentityBuilder AddAspNetMembershipBackedAspNetIdentity(this IServiceCollection services, string connectionString)
         {
             services.AddDbContext<AspNetMembershipDbContext>(options => options.UseSqlServer(connectionString));
             services.AddTransient<IPasswordHasher<AspNetMembershipUser>, AspNetMembershipPasswordHasher>();
 
-            return services.AddIdentity<AspNetMembershipUser, IdentityRole>(options =>
-            {
-                options.ClaimsIdentity.UserIdClaimType = JwtClaimTypes.Subject;
-                options.ClaimsIdentity.UserNameClaimType = JwtClaimTypes.Name;
-            })
+            return services.AddIdentity<AspNetMembershipUser, IdentityRole>()
+                .AddEntityFrameworkStores<AspNetMembershipDbContext>()
+                .AddUserStore<AspNetMembershipUserStore>()
+                .AddDefaultTokenProviders();
+        }
+
+        public static IdentityBuilder AddAspNetMembershipBackedAspNetIdentity(this IServiceCollection services, string connectionString, Action<IdentityOptions> setupAction)
+        {
+            services.AddDbContext<AspNetMembershipDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddTransient<IPasswordHasher<AspNetMembershipUser>, AspNetMembershipPasswordHasher>();
+
+            return services.AddIdentity<AspNetMembershipUser, IdentityRole>(setupAction)
                 .AddEntityFrameworkStores<AspNetMembershipDbContext>()
                 .AddUserStore<AspNetMembershipUserStore>()
                 .AddDefaultTokenProviders();
